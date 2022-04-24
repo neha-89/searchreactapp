97c572e68ec096bda0506cec5fda122dad3e89ad
Neha Sontakke <wasnik.neha@gmail.com>
2022-04-24 23:25:11 +0530

    added the folder

---
+++ search/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+        <center>  
+            <p>Developer</p>
+        </center>
+       
+        </>
+    )
+}
---
+++ search/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+         <input type = "text"/>
+         <div>
+             <p>user input text</p>
+         </div>
+        </>
+    )
+}
