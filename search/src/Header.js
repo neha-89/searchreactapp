3b707b6be0439c4feeb9bbae55a9fe5780dcb868
Neha Sontakke <wasnik.neha@gmail.com>
2022-04-30 22:46:37 +0530

    added the class based component

--- search/src/Header.js
+++ search/src/Header.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
-const Header = ()=>{
-    return(
-        <center>
-        <div>
-            <h1>Developer</h1>
-        </div>
+class Header extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            heading:"React App",
+            keyword:"User Text Here"
+        }
+    }
+    render(){
+        return(
+            <>
+            <header>
+            <div className="logo">{this.state.heading}</div>
+            <center>
+                <input />
+                <div style={{color:'white',fontSize:'20px'}}>{this.state.keyword}</div>
+            </center>
+        </header>
         <hr/>
-        </center>
-    )
+        </>
+        )
+    }
+    
 }
 
 export default Header;
