d372d56a5ed7877095263e478dfd7a83ab282cde
Neha Sontakke <wasnik.neha@gmail.com>
2022-05-16 18:00:48 +0530

    used state for updateing the productdisplay

--- search/src/Header.js
+++ search/src/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import "./Header.css";
 class Header extends React.Component {
     constructor(){
         super()
@@ -8,13 +8,21 @@ class Header extends React.Component {
             keyword:"User Text Here"
         }
     }
+    handleChange = (event) => {
+        console.log(event.target.value)
+        this.setState({keyword:event.target.value?event.target.value:'User Text Here'})
+        // from here we are passing data
+       // this.props.userText(event.target.value)
+
+    }
+
     render(){
         return(
             <>
             <header>
             <div className="logo">{this.state.heading}</div>
             <center>
-                <input />
+                <input onChange = {this.handleChange}/>
                 <div style={{color:'white',fontSize:'20px'}}>{this.state.keyword}</div>
             </center>
         </header>
--- search/src/Home.js
+++ search/src/Home.js
@@ -1,17 +1,30 @@
 import React from 'react';
-
+import JSON from './db.json';
 import Header from "./Header";
 import Footer from "./Footer";
+import Product from './productDisplay';
+import { Component } from 'react';
 
-const Home = () => {
+class Home extends Component {
+   constructor(){
+       super()
+       
+       this.state = {
+           productData : JSON,
+          
+       }
+   }
+   
+   render(){
+       console.log(this.state.productData)
     return (
         <>
-        <Header/>
-        <h1>Hii From React</h1>
+        <Header  />
+        <Product prodData = {this.state.productData} />
         <Footer year ="2022 "month = "April"/>
         </>
     )// as one function can return one value at a time so we need to wrap it under single html element.
-        
+  }
 }
 
-export default Home
+export default Home ;
