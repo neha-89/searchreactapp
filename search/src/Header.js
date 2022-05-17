71dc39ba46add36db96d268ec0181b19afc0f4f7
Neha Sontakke <wasnik.neha@gmail.com>
2022-05-17 22:38:14 +0530

    updated

--- search/src/Header.js
+++ search/src/Header.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import "./Header.css";
-class Header extends React.Component {
+import React,{Component,Fragment} from 'react';
+import './Header.css';
+
+class Header extends Component{
     constructor(){
         super()
-        this.state = {
-            heading:"React App",
-            keyword:"User Text Here"
+        console.log("inside constructor ")
+        this.state={
+            heading:'React App',
+            keyword:'User Text Here'
         }
     }
+
     handleChange = (event) => {
         console.log(event.target.value)
         this.setState({keyword:event.target.value?event.target.value:'User Text Here'})
@@ -17,20 +20,20 @@ class Header extends React.Component {
     }
 
     render(){
-        return(
-            <>
-            <header>
-            <div className="logo">{this.state.heading}</div>
-            <center>
-                <input onChange = {this.handleChange}/>
-                <div style={{color:'white',fontSize:'20px'}}>{this.state.keyword}</div>
-            </center>
-        </header>
-        <hr/>
-        </>
+        console.log("inside render")
+        return (
+            <Fragment>
+                <header>
+                    <div className="logo">{this.state.heading}</div>
+                    <center>
+                        <input onChange={this.handleChange}/>
+                        <div style={{color:'white',fontSize:'20px'}}>{this.state.keyword}</div>
+                    </center>
+                </header>
+                <hr/>
+            </Fragment>
         )
     }
-    
 }
 
 export default Header;
--- search/src/Home.js
+++ search/src/Home.js
@@ -1,38 +1,76 @@
-import React from 'react';
+// import React from 'react';
+// import JSON from './db.json';
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import Product from './productDisplay';
+// import { Component } from 'react';
+
+// class Home extends Component {
+//    constructor(){
+//        super()
+       
+//        this.state = {
+//            productData : JSON,
+//            filteredData : JSON
+          
+//        }
+//    }
+
+//    filterProduct = (keyword) => {
+//     let output = this.state.productData.filter((item) => {
+//         return item.name.toLowerCase().indexOf(keyword.toLowerCase())>-1
+//     })
+//     this.setState({filteredData:output})
+// }
+//    render(){
+//        //console.log(this.state.productData)
+//     return (
+//         <>
+//         <Header usertext = {(data)=>{this.filterProduct(data)}}  />{/*receving the data*/}
+//         <Product prodata = {this.state.filteredData} />
+//         {/* <Product prodData = {this.state.productData} /> */}
+//         <Footer year ="2022 "month = "April"/>
+//         </>
+//     )// as one function can return one value at a time so we need to wrap it under single html element.
+//   }
+// }
+
+// export default Home ;
+import React,{Component} from 'react';
+import Header from './Header';
+import Footer from './Footer';
 import JSON from './db.json';
-import Header from "./Header";
-import Footer from "./Footer";
-import Product from './productDisplay';
-import { Component } from 'react';
+import Product from './productDisplay'
 
 class Home extends Component {
-   constructor(){
-       super()
-       
-       this.state = {
-           productData : JSON,
-           filteredData : JSON
-          
-       }
-   }
+    constructor(){
+        super()
 
-   filterProduct = (keyword) => {
-    let output = this.state.productData.filter((item) => {
-        return item.name.toLowerCase().indexOf(keyword.toLowerCase())>-1
-    })
-    this.setState({filteredData:output})
-}
-   render(){
-       //console.log(this.state.productData)
-    return (
-        <>
-        <Header usertext = {(data)=>{this.filterProduct(data)}}  />{/*receving the data*/}
-        <Product prodata = {this.state.filteredData} />
-        {/* <Product prodData = {this.state.productData} /> */}
-        <Footer year ="2022 "month = "April"/>
-        </>
-    )// as one function can return one value at a time so we need to wrap it under single html element.
-  }
+        this.state={
+            productData:JSON,
+            filteredData:JSON
+        }
+    }
+    /* var a= [1,3,6,8,2]
+    a.filter((data) => {return data>5})*/
+
+    filterProduct = (keyword) => {
+        let output = this.state.productData.filter((item) => {
+            return item.name.toLowerCase().indexOf(keyword.toLowerCase())>-1
+        })
+        this.setState({filteredData:output})
+    }
+
+    render(){
+        //console.log(this.state.productData)
+        return(
+            <>
+                <Header userText={(data) => {this.filterProduct(data)}}/>
+                <Product prodData={this.state.filteredData}/>
+                <Footer year="2022" month="April"/>
+            </>
+        )
+    }
 }
 
-export default Home ;
+export default Home;
